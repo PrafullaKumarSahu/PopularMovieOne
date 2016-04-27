8cc5fb40d1ea60120ecdb56cc60e4c8be4b76533
prafullakumarsahu <prafullakumarsahu@gmail.com>
2016-04-27 21:08:57 +0530

    Adds fake data and creats grid

--- app/src/main/java/com/android/serverwarrior/popularmoviesone/MovieListFragment.java
+++ app/src/main/java/com/android/serverwarrior/popularmoviesone/MovieListFragment.java
@@ -1,22 +1,41 @@
 package com.android.serverwarrior.popularmoviesone;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MovieListFragment extends Fragment {
 
+    public ImageAdapter  mMoviePosterAdapter;
     public MovieListFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        GridView gridview = (GridView) rootView.findViewById(R.id.movie_gridview);
+        gridview.setAdapter(new ImageAdapter(getContext()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getContext(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
     }
 }
