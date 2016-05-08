655d2d1f011038de0009b1bb8a16cdce05cea7f9
prafullakumarsahu <prafullakumarsahu@gmail.com>
2016-05-08 19:46:30 +0530

    Removes floating action button

--- app/src/main/java/com/android/serverwarrior/popularmoviesone/MovieDetailActivity.java
+++ app/src/main/java/com/android/serverwarrior/popularmoviesone/MovieDetailActivity.java
@@ -2,13 +2,10 @@ package com.android.serverwarrior.popularmoviesone;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
@@ -28,14 +25,7 @@ public class MovieDetailActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
