4b4d5771d1483b9ede87cc37e1c99be3edad0359
prafullakumarsahu <prafullakumarsahu@gmail.com>
2016-05-29 04:14:52 +0530

    Adds swipe refresh interface and settign item to refresh (XML)

--- app/src/main/java/com/android/serverwarrior/popularmoviesone/MainActivity.java
+++ app/src/main/java/com/android/serverwarrior/popularmoviesone/MainActivity.java
@@ -1,7 +1,6 @@
 package com.android.serverwarrior.popularmoviesone;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -12,7 +11,6 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     final static int REQ_CODE = 1;
     Movie movies;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,12 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
+/*    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-    }
+    }*/
 }
